d31ba0b20a7ac75af075d8afa85e145d1aba3c0e
Leon Shimizu <lmshimizu@gmail.com>
2024-08-21 01:56:49 +1000

    Add console logs to the axios.js to print out the baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000;' // Default to localhost if no env variable is set
+console.log('Using API URL:', axios.defaults.baseURL);
+console.log(`baseURL: ${baseURL}`)
 
 const instance = axios.create({
   baseURL: baseURL
