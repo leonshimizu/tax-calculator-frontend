df9e639615508773eb503f198e6724bd4f1c209e
Leon Shimizu <lmshimizu@gmail.com>
2024-08-22 03:45:17 +1000

    Update axios.js logic to change the baseURL based on what envirnment is being used

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 // src/api/axios.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000'; // Default to localhost if no env variable is set
+// Determine the base URL based on the environment
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000' // Use localhost when in development mode
+    : process.env.REACT_APP_API_URL || ''; // Use the environment variable or leave empty for production
 
 const instance = axios.create({
   baseURL: baseURL
