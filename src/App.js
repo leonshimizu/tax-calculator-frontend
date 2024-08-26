5515920db61eda5b524153038025bc501dfcadf0
“leonshimizu” <lmshimizu@gmail.com>
2024-08-26 14:49:32 +1000

    Update app to route to the home page whenever a user refreshes the page - temp fix for 'Not Found' (refresh) issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PayrollHome from './components/PayrollHome';
 import EmployeeList from './components/EmployeeList';
@@ -13,32 +13,47 @@ import PayrollFileUpload from './components/PayrollFileUpload';
 import NotFoundPage from './components/NotFoundPage';
 import CustomColumnsManager from './components/CustomColumnsManager';
 
-function App() {
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.performance) {
+      if (performance.getEntriesByType("navigation")[0].type === "reload") {
+        // Navigate to home page on refresh
+        window.location.href = "/";
+      }
+    }
+  }, [location]);
+
   return (
-    <Router>
-      <div style={{ paddingTop: '60px' }}>
-        <Routes>
-          {/* Define all the routes for your application */}
-          <Route path="/" element={<><Navbar /><PayrollHome /></>} />
-          <Route path="/companies" element={<><Navbar /><PayrollHome /></>} />
-          <Route path="/companies/:companyId/employees" element={<><Navbar /><EmployeeList /></>} />
-          <Route path="/companies/:companyId/employees/:employeeId" element={<><Navbar /><EmployeeDetail /></>} />
-          <Route path="/companies/:companyId/employees/batch" element={<><Navbar /><BatchPayrollEntry /></>} />
-          <Route path="/companies/:companyId/batch-payroll-records-display" element={<><Navbar /><BatchPayrollRecordsDisplay /></>} />
-          <Route path="/companies/:companyId/employees/:employeeId/payroll_records/:recordId" element={<><Navbar /><PayrollRecordDetails /></>} />
-          <Route path="/companies/:companyId/employees/:employeeId/payroll_records/new" element={<><Navbar /><CreatePayrollRecord /></>} />
-          <Route path="/companies/:companyId/employees/upload" element={<><Navbar /><EmployeeFileUpload /></>} />
-          <Route path="/companies/:companyId/payroll_records/upload" element={<><Navbar /><PayrollFileUpload /></>} />
+    <div style={{ paddingTop: '60px' }}>
+      <Routes>
+        {/* Define all the routes for your application */}
+        <Route path="/" element={<><Navbar /><PayrollHome /></>} />
+        <Route path="/companies" element={<><Navbar /><PayrollHome /></>} />
+        <Route path="/companies/:companyId/employees" element={<><Navbar /><EmployeeList /></>} />
+        <Route path="/companies/:companyId/employees/:employeeId" element={<><Navbar /><EmployeeDetail /></>} />
+        <Route path="/companies/:companyId/employees/batch" element={<><Navbar /><BatchPayrollEntry /></>} />
+        <Route path="/companies/:companyId/batch-payroll-records-display" element={<><Navbar /><BatchPayrollRecordsDisplay /></>} />
+        <Route path="/companies/:companyId/employees/:employeeId/payroll_records/:recordId" element={<><Navbar /><PayrollRecordDetails /></>} />
+        <Route path="/companies/:companyId/employees/:employeeId/payroll_records/new" element={<><Navbar /><CreatePayrollRecord /></>} />
+        <Route path="/companies/:companyId/employees/upload" element={<><Navbar /><EmployeeFileUpload /></>} />
+        <Route path="/companies/:companyId/payroll_records/upload" element={<><Navbar /><PayrollFileUpload /></>} />
 
-          {/* Add routes for managing custom columns */}
-          <Route path="/companies/:companyId/custom_columns" element={<><Navbar /><CustomColumnsManager /></>} />
+        {/* Add routes for managing custom columns */}
+        <Route path="/companies/:companyId/custom_columns" element={<><Navbar /><CustomColumnsManager /></>} />
 
-          {/* Catch-all route to redirect to home page */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
-    </Router>
+        {/* Catch-all route to redirect to home page */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   );
-}
+};
 
-export default App;
+const Main = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default Main;
